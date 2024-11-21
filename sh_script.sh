198c6bec04961c5bbae9ec07bf5da5f3978735b8
EdgarLuna <eluna@urchinsys.com>
2024-11-21 16:55:06 +0100

    modifying script

--- sh_script.sh
+++ sh_script.sh
@@ -12,13 +12,13 @@ then
   sed -i "s/{{MYSQL_ROOT_PASSWORD}}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32)/g" ./.env
   sed -i "s/{{MYSQL_PASSWORD}}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32)/g" ./.env
   sed -i "s/{{EASY_WG_PASSWORD}}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32)/g" ./.env
-
 fi
 
 if [[ -n $(grep -E -ir --include=.env "{{CROWDSEC_BOUNCER_APIKEY}}" .) ]]
 then
   CROWD_SEC_ENROLLMENT_KEY=$1 docker compose up crowdsec -d
-  sed -i "s/{{CROWDSEC_BOUNCER_APIKEY}}/$(docker compose exec crowdsec cscli bouncer add npm-bouncer)/g" ./.env
+  sleep 10
+  sed -i "s/{{CROWDSEC_BOUNCER_APIKEY}}/$(docker exec crowdsec cscli bouncer add npm-bouncer | grep -A 2 "API key for 'npm-bouncer':" | tail -n 1 | xargs)/g" ./.env
   docker compose down
 fi
 
@@ -26,9 +26,4 @@ set -a
 source ./.env
 export CROWD_SEC_ENROLLMENT_KEY=$1
 export WG_HOST=$2
-MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
-MYSQL_PASSWORD="$MYSQL_PASSWORD" \
-CROWDSEC_BOUNCER_APIKEY="$CROWDSEC_BOUNCER_APIKEY" \
-CROWD_SEC_ENROLLMENT_KEY="$CROWD_SEC_ENROLLMENT_KEY" \
-WG_HOST="$WG_HOST" \
-docker compose up -d
+MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" MYSQL_PASSWORD="$MYSQL_PASSWORD" EASY_WG_PASSWORD="$EASY_WG_PASSWORD" CROWDSEC_BOUNCER_APIKEY="$CROWDSEC_BOUNCER_APIKEY" CROWD_SEC_ENROLLMENT_KEY="$CROWD_SEC_ENROLLMENT_KEY" WG_HOST="$WG_HOST" docker compose up -d
